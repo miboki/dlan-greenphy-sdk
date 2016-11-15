fd6673ede85292d22f3de458631b7d97ba169916
Michael Lamberti <michael.lamberti@devolo.de>
2016-11-15 15:36:10 +0100

    Temporary fix to build FirmwareStandalone

--- ApplicationSupport/src/JSON/value.c
+++ ApplicationSupport/src/JSON/value.c
@@ -867,8 +867,9 @@ double json_real_value(const json_t *json)
 
 int json_real_set(json_t *json, double value)
 {
-    if(!json_is_real(json) || isnan(value) || isinf(value))
-        return -1;
+/* XXX: fix missing implementation or change json library */
+//    if(!json_is_real(json) || isnan(value) || isinf(value))
+//        return -1;
 
     json_to_real(json)->value = value;
 
