4d918607c3c3fe3a8307feb0eb76312b57802408
Michael Lamberti <michael.lamberti@devolo.de>
2017-08-04 18:29:09 +0200

    Removed old ApplicationSupport / FirmwareImageForTFTP code

--- ApplicationSupport/bin/makeit.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-echo !!!!unzip!!!!
-ren lwip_lpc\lwip-1.4.0 lwip_lpc\lwip
-cd lwip_lpc\contrib-1.4.0\apps\httpserver_raw\makefsdata
-cl /EHsc /DWIN32 /I..\..\..\..\lwip\src\include /I ..\..\..\ports\win32\include /I..\..\..\..\lwip\src\include\ipv4 makefsdata.c 
--- ApplicationSupport/inc/CLI.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLI_H_
-#define CLI_H_
-
-#include "greenPhyModuleApplication.h"
-
-void cliInit(void);
-
-#endif /* CLI_H_ */
