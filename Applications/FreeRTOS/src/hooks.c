41ce46e0ec8cd88d7cbc595a760baf5a8974aebf
caroline toebben <caroline.toebben@devolo.de>
2020-11-05 12:14:55 +0100

    Updated FreeRTOS to v10.4.1

--- Applications/FreeRTOS/src/hooks.c
+++ Applications/FreeRTOS/src/hooks.c
@@ -58,7 +58,7 @@ void vApplicationIdleHook(void) {
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName) {
+void vApplicationStackOverflowHook(xTaskHandle pxTask, char *pcTaskName) {
 	/* This function will get called if a task overflows its stack. */
 
 	(void) pxTask;
